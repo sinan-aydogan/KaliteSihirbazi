850ade8febfe1daf96ebaa4ecd598fbd44e8771e
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2022-03-12 13:56:14 +0300

    Nunito font ailesini Oswald ile değiştirir

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ['Oswald', ...defaultTheme.fontFamily.sans],
             },
         },
     },
