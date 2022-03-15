1fcebd1fa9c5732e70df3c36c29a09a4c16e7f0a
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2022-03-16 00:42:16 +0300

    modüllerin baz database yapılarını ekler

---
+++ database/migrations/2022_03_13_172118_create_take_time_offs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('take_time_offs', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('take_time_offs');
+    }
+};
