3483c57b39bb0b214cf3564ebf7880005611e553
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2022-03-20 21:10:34 +0300

    content card komponentini ekler

---
+++ resources/js/Layouts/ContentCard.vue
@@ -0,0 +1,44 @@
+<template>
+    <!--Top Area-->
+    <div class="mb-2">
+        <!--Titles-->
+        <div class="flex flex-col justify-start">
+            <!--Title-->
+            <h2 v-if="title" class="text-2xl font-semibold">
+                {{title}}
+            </h2>
+            <!--Sub Title-->
+            <h5 v-if="subTitle" class="text-sm text-slate-400">
+                {{subTitle}}
+            </h5>
+        </div>
+        <!--Action Area-->
+        <div>
+
+        </div>
+    </div>
+    <!--Content-->
+    <div class="p-4 bg-white dark:bg-slate-800/50 rounded-lg shadow border dark:border-slate-600">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ContentCard",
+    props: {
+        title: {
+            type: String,
+            default: null
+        },
+        subTitle: {
+            type: String,
+            default: null
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
