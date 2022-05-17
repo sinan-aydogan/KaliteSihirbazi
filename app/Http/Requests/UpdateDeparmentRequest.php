3fd5b34d3342a7ead9051c27aea0d4eeb7a581e7
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2022-05-18 01:38:05 +0300

    Adds validation for UpdateDeparmentRequest.php

--- app/Http/Requests/UpdateDeparmentRequest.php
+++ app/Http/Requests/UpdateDeparmentRequest.php
@@ -13,7 +13,7 @@ class UpdateDeparmentRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return auth()->check();
     }
 
     /**
@@ -24,7 +24,8 @@ class UpdateDeparmentRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'code' => 'required|unique:departments|max:10',
+            'name' => 'required|max:100'
         ];
     }
 }
