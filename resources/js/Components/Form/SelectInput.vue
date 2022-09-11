0080ff898cc60737c331eca8b37ad14c672043c8
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2022-09-11 23:50:46 +0300

    fixes selected id error when update - updates translates

--- resources/js/Components/Form/SelectInput.vue
+++ resources/js/Components/Form/SelectInput.vue
@@ -1,29 +1,29 @@
 <script setup>
-import { ref, onBeforeMount } from "vue";
-import { onClickOutside } from "@vueuse/core";
+import {ref} from "vue";
+import {onClickOutside} from "@vueuse/core";
 
 const props = defineProps({
-    modelValue: {
-        type: [String, Number, Date],
-        default: null
-    },
-    options: {
-        type: Array,
-        default: () => []
-    },
-    optionKey: {
-        type: String,
-        default: "id"
-    },
-    optionLabel: {
-        type: String,
-        default: "label"
-    },
-    selectText: {
-        type: String,
-        default: ''
-    },
-    disabled: Boolean
+  modelValue: {
+    type: [String, Number, Date],
+    default: null
+  },
+  options: {
+    type: Array,
+    default: () => []
+  },
+  optionKey: {
+    type: String,
+    default: "id"
+  },
+  optionLabel: {
+    type: String,
+    default: "label"
+  },
+  selectText: {
+    type: String,
+    default: ''
+  },
+  disabled: Boolean
 });
 
 const emit = defineEmits(['update:modelValue'])
@@ -35,78 +35,72 @@ onClickOutside(target, (event) => showOptions.value = false);
 /*Show Options*/
 const showOptions = ref();
 const toggle = () => {
-    showOptions.value = !showOptions.value
+  showOptions.value = !showOptions.value
 }
 
 /*Select*/
-const selected = ref();
 const select = (option) => {
-    selected.value = option;
+  if(option){
     emit("update:modelValue", option[props.optionKey]);
     showOptions.value = false;
+  }
 }
-
-onBeforeMount(() => {
-    if (props.modelValue) {
-        select(props.options.find(i => i[props.optionKey] === props.modelValue))
-    }
-})
 
 /*Clear*/
 const clear = () => {
-    showOptions.value = false;
-    selected.value = null;
-    emit("update:modelValue", null);
+  showOptions.value = false;
+  emit("update:modelValue", null);
 }
 </script>
 
 <template>
-    <div class="relative space-y-1 gap-1 min-w-[12rem]" ref="target">
-        <!--Place holder-->
-        <div @click="toggle"
-            class="flex justify-between items-center space-x-2 h-10 border px-4 py-2 rounded-lg cursor-pointer select-none"
-            :class="{
+  <div class="relative space-y-1 gap-1 min-w-[12rem]" ref="target">
+    <!--Place holder-->
+    <div @click="toggle"
+         class="flex justify-between items-center space-x-2 h-10 border px-4 py-2 rounded-lg cursor-pointer select-none"
+         :class="{
                 'opacity-50 pointer-events-none': disabled
             }">
-            <div class="block overflow-hidden whitespace-nowrap">
-                <!--Placeholder-->
-                <span v-if="!modelValue"
-                    v-text="selectText.length > 0 ? selectText : $t('form.selectInput.selectOption')"></span>
-                <!--Selected-->
-                <div v-else>
-                    <slot v-if="$slots[selected[optionKey]]" :name="selected[optionKey]"></slot>
-                    <span v-else>{{  selected[optionLabel]  }}</span>
-                </div>
-            </div>
-
-            <!--Trigger Icons-->
-            <div class="flex items-center space-x-2">
-                <!--Clear Button-->
-                <font-awesome-icon v-if="selected" @click.stop="clear" icon="circle-xmark"
-                    class="hover:text-rose-600 active:scale-90 duration-100" />
-                <!--Trigger-->
-                <font-awesome-icon icon="chevron-down" />
-            </div>
+      <div class="block overflow-hidden whitespace-nowrap">
+        <!--Placeholder-->
+        <span v-if="!modelValue"
+              v-text="selectText.length > 0 ? selectText : t('action.select')"></span>
+        <!--Selected-->
+        <div v-else>
+          <slot v-if="$slots.hasOwnProperty('selected')" name="selected" :props="options.find(i=>i[optionKey] === modelValue)"></slot>
+          <span v-else>{{ options.find(i=>i[optionKey] === modelValue)[optionLabel] }}</span>
         </div>
+      </div>
 
-        <!--Selected Content-->
-
-        <!--Options-->
-        <div v-if="showOptions"
-            class="absolute z-50 bg-white dark:bg-slate-600 border shadow-lg rounded-lg py-1 w-full">
-            <template v-for="option in options" :key="option">
-                <div @click="select(option)"
-                    class="flex justify-between items-center border-b last:border-b-0 last:rounded-b first:rounded-t py-1 px-2 hover:bg-slate-100 dark:hover:bg-slate-800/50 cursor-pointer">
-                    <!--Label-->
-                    <div>
-                        <slot v-if="$slots[option[optionKey]]" :name="option[optionKey]" :props="option"></slot>
-                        <span v-else v-text="option[optionLabel]"></span>
-                    </div>
-
-                    <!--Selected Indicator-->
-                    <font-awesome-icon icon="circle-check" v-if="option[optionKey] === modelValue" />
-                </div>
-            </template>
-        </div>
+      <!--Trigger Icons-->
+      <div class="flex items-center space-x-2">
+        <!--Clear Button-->
+        <font-awesome-icon
+            v-if="modelValue"
+            @click.stop="clear"
+            icon="circle-xmark"
+            class="hover:text-rose-600 active:scale-90 duration-100"/>
+        <!--Trigger-->
+        <font-awesome-icon icon="chevron-down"/>
+      </div>
     </div>
+
+    <!--Options-->
+    <div v-if="showOptions"
+         class="absolute z-50 bg-white dark:bg-slate-600 border shadow-lg rounded-lg py-1 w-full">
+      <template v-for="option in options" :key="option">
+        <div @click="select(option)"
+             class="flex justify-between items-center border-b last:border-b-0 last:rounded-b first:rounded-t py-1 px-2 hover:bg-slate-100 dark:hover:bg-slate-800/50 cursor-pointer">
+          <!--Label-->
+          <div>
+            <slot v-if="$slots.hasOwnProperty('option')" name="option" :props="option"></slot>
+            <span v-else v-text="option[optionLabel]"></span>
+          </div>
+
+          <!--Selected Indicator-->
+          <font-awesome-icon icon="circle-check" v-if="option[optionKey] === modelValue"/>
+        </div>
+      </template>
+    </div>
+  </div>
 </template>
