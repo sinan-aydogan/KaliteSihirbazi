1774fa3fc93e2c77508cc6a28fdaac609b4372e5
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2022-09-22 01:18:11 +0300

    fixes Boolean value errors what false value

--- resources/js/Components/Form/SelectInput.vue
+++ resources/js/Components/Form/SelectInput.vue
@@ -4,7 +4,7 @@ import {onClickOutside} from "@vueuse/core";
 
 const props = defineProps({
   modelValue: {
-    type: [String, Number, Date],
+    type: [String, Number, Date, Boolean],
     default: null
   },
   options: {
@@ -54,20 +54,21 @@ const clear = () => {
 </script>
 
 <template>
-  <div class="relative space-y-1 gap-1 min-w-[12rem]" ref="target">
+  <div class="relative space-y-1 gap-1" ref="target">
     <!--Place holder-->
     <div @click="toggle"
-         class="flex justify-between items-center space-x-2 h-10 border px-4 py-2 rounded-lg cursor-pointer select-none"
+         class="flex justify-between items-center space-x-2 h-10 dark:bg-slate-900/30 border border-slate-300 dark:border-slate-600 px-4 py-2 rounded-lg cursor-pointer select-none w-full"
          :class="{
                 'opacity-50 pointer-events-none': disabled
             }">
       <div class="block overflow-hidden whitespace-nowrap">
         <!--Placeholder-->
-        <span v-if="!modelValue"
+        <span v-if="typeof modelValue === 'boolean' ? false : !modelValue"
               v-text="selectText.length > 0 ? selectText : t('action.select')"></span>
         <!--Selected-->
         <div v-else>
           <slot v-if="$slots.hasOwnProperty('selected')" name="selected" :props="options.find(i=>i[optionKey] === modelValue)"></slot>
+          <slot v-else-if="$slots.hasOwnProperty('both')" name="both" :props="options.find(i=>i[optionKey] === modelValue)"></slot>
           <span v-else>{{ options.find(i=>i[optionKey] === modelValue)[optionLabel] }}</span>
         </div>
       </div>
@@ -76,7 +77,7 @@ const clear = () => {
       <div class="flex items-center space-x-2">
         <!--Clear Button-->
         <font-awesome-icon
-            v-if="modelValue"
+            v-if="typeof modelValue === 'boolean' ? true : modelValue"
             @click.stop="clear"
             icon="circle-xmark"
             class="hover:text-rose-600 active:scale-90 duration-100"/>
@@ -86,21 +87,22 @@ const clear = () => {
     </div>
 
     <!--Options-->
-    <div v-if="showOptions"
-         class="absolute z-50 bg-white dark:bg-slate-600 border shadow-lg rounded-lg py-1 w-full">
-      <template v-for="option in options" :key="option">
-        <div @click="select(option)"
-             class="flex justify-between items-center border-b last:border-b-0 last:rounded-b first:rounded-t py-1 px-2 hover:bg-slate-100 dark:hover:bg-slate-800/50 cursor-pointer">
-          <!--Label-->
-          <div>
-            <slot v-if="$slots.hasOwnProperty('option')" name="option" :props="option"></slot>
-            <span v-else v-text="option[optionLabel]"></span>
-          </div>
+      <div v-if="showOptions"
+           class="absolute z-50 bg-white dark:bg-slate-600 border shadow-lg rounded-md py-1 w-full">
+        <template v-for="option in options" :key="option">
+          <div @click="select(option)"
+               class="flex justify-between items-center border-b last:border-b-0 last:rounded-b first:rounded-t py-1 px-2 hover:bg-slate-100 dark:hover:bg-slate-800/50 cursor-pointer transition-all">
+            <!--Label-->
+            <div>
+              <slot v-if="$slots.hasOwnProperty('option')" name="option" :props="option"></slot>
+              <slot v-else-if="$slots.hasOwnProperty('both')" name="both" :props="option"></slot>
+              <span v-else v-text="option[optionLabel]"></span>
+            </div>
 
-          <!--Selected Indicator-->
-          <font-awesome-icon icon="circle-check" v-if="option[optionKey] === modelValue"/>
-        </div>
-      </template>
-    </div>
+            <!--Selected Indicator-->
+            <font-awesome-icon icon="circle-check" v-if="option[optionKey] === modelValue"/>
+          </div>
+        </template>
+      </div>
   </div>
 </template>
