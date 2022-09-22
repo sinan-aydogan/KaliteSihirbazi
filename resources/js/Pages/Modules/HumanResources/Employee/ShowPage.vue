849a4f6cbfab5310f939d08c3e1686a6505fb428
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2022-09-23 00:23:09 +0300

    adds skeleton of the employee detail page

--- resources/js/Pages/Modules/HumanResources/Employee/ShowPage.vue
+++ resources/js/Pages/Modules/HumanResources/Employee/ShowPage.vue
@@ -3,79 +3,124 @@ import AppLayout from "@/Layouts/AppLayout.vue";
 import {Link} from "@inertiajs/inertia-vue3";
 import {Inertia} from "@inertiajs/inertia";
 
+const selectedTab = ref(PersonalInfo);
+
+// Pages
+import PersonalInfo from './Pages/PersonalInfo.vue'
+import EmploymentInfo from './Pages/EmploymentInfo.vue'
+import TimeOff from './Pages/TimeOff.vue'
+import CareerPathing from './Pages/CareerPathing.vue'
+import Debt from './Pages/Debt.vue'
+import Education from './Pages/Education.vue'
+
+
 // Components
 import SimpleButton from "@/Components/Button/SimpleButton.vue"
-import Alert from "@/Components/Alert/Alert.vue";
+import Avatar from "@/Components/Avatar/Avatar.vue";
 
 // Props
 const props = defineProps({
   data: Object
 })
 
+// Provide Data
+provide('data', props.data);
+
 // Multi-lang
 import Translates from "./translates"
-const {t,tm} = Translates();
+import {provide, ref} from "vue";
 
-const handleDelete = ()=>{
-  Inertia.delete(route('department.destroy', props.data.id))
+const {t, tm} = Translates();
+
+/*Tabs*/
+const tabs = [
+  {
+    id: 'personalInfo',
+    label: tm('term.personalInfo'),
+    page: PersonalInfo
+  },
+  {
+    id: 'employmentInfo',
+    label: tm('term.employmentInfo'),
+    page: EmploymentInfo
+  },
+  {
+    id: 'careerPathing',
+    label: t('term.careerPathing'),
+    page: CareerPathing
+  },
+  {
+    id: 'timeOff',
+    label: t('term.timeOff'),
+    page: TimeOff
+  },
+  {
+    id: 'debt',
+    label: t('term.debt'),
+    page: Debt
+  },
+  {
+    id: 'education',
+    label: t('term.education'),
+    page: Education
+  }
+]
+
+const handleDelete = () => {
+  Inertia.delete(route('employee.destroy', props.data.id))
 }
 </script>
 
 <template>
-<app-layout :title="data.name">
+  <app-layout :title="data.name">
 
-  <template #actionArea>
-    <simple-button @click="handleDelete" color="red">
-      <font-awesome-icon icon="trash-can" class="mr-2" />
-      <span v-text="tm('action.delete')" />
-    </simple-button>
+    <template #actionArea>
+      <simple-button @click="handleDelete" color="red">
+        <font-awesome-icon icon="trash-can" class="mr-2"/>
+        <span v-text="tm('action.delete')"/>
+      </simple-button>
 
-    <simple-button type="route" :link="route('department.index')">
-      <font-awesome-icon icon="fa-solid fa-left-long" class="mr-2" />
-      <span v-text="tm('action.goBack')" />
-    </simple-button>
-  </template>
+      <simple-button type="route" :link="route('employee.index')">
+        <font-awesome-icon icon="fa-solid fa-left-long" class="mr-2"/>
+        <span v-text="tm('action.goBack')"/>
+      </simple-button>
+    </template>
 
-  <template #header>
-    <!--Department Name-->
-    <div class="flex items-center space-x-4">
-      <!--Name-->
-      <span v-text="data.name" class="font-semibold text-xl text-slate-800 dark:text-slate-200 leading-none"/>
-      <!--Code-->
-      <div class="flex space-x-1 items-center border px-2 py-1 rounded-lg">
-        <font-awesome-icon icon="fa-solid fa-fingerprint"/>
-        <span v-text="data.code" class="text-sm uppercase"/>
+    <template #header>
+      <div class="flex space-x-4">
+        <!--Profile Photo-->
+        <div v-if="data['account']"  class="-ml-2">
+          <avatar :src="data.account.profile_photo_url" size="lg"/>
+        </div>
+        <div class="flex flex-col justify-center">
+          <!--Employee Name-->
+          <span v-text="data.employeeName" class="font-semibold text-xl text-slate-800 dark:text-slate-200 leading-none"/>
+          <!--Department-->
+          <Link v-if="data['department']">
+            <span v-text="data.department.name"></span>
+          </Link>
+        </div>
+      </div>
+    </template>
+
+    <div>
+      <!--Tabs-->
+      <div class="flex">
+        <template v-for="i in tabs">
+          <div @click="selectedTab = i.page"
+               class="flex justify-center items-center border border-r-0 first:rounded-l-md bg-slate-700 hover:bg-rose-700/10 hover:border-rose-400 px-4 py-2 whitespace-nowrap transition cursor-pointer">
+            <span v-text="i.label"></span>
+          </div>
+        </template>
+        <div class="flex w-full border rounded-r-md"></div>
+      </div>
+
+      <!--Content-->
+      <div>
+        <component :is="selectedTab"/>
       </div>
     </div>
-    <!--Manager-->
-    <div v-if="data['manager']" class="flex items-center space-x-1 text-xs mt-1">
-      <font-awesome-icon icon="fa-solid fa-user-shield" :class="`before:content-['${tm('term.manager')}']`"/>
-      <!--Manager Name-->
-      <Link :href="route('employee.show', data.manager.id)" v-text="data.manager.employeeName"/>
-    </div>
-    <div v-else>
-      {{tm('term.noManagerAppointed')}}
-    </div>
-  </template>
 
-  <div class="grid grid-cols-12">
 
-    <!--Employee List-->
-    <div v-if="data['employees']">
-      Employee List
-    </div>
-    <div v-else class="col-span-12 space-y-2">
-      <!--Alert-->
-      <Alert>
-        {{tm('message.feedback.noAttachedEmployee')}}
-      </Alert>
-      <!--Add Employee-->
-      <simple-button full-size color="green">
-        <font-awesome-icon icon="fa-solid fa-user-plus"/>
-        <span v-text="tm('action.attachEmployee')"/>
-      </simple-button>
-    </div>
-  </div>
-
-</app-layout>
+  </app-layout>
 </template>
