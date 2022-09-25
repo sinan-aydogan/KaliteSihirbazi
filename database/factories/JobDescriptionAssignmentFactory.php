cc16495eb47fec650822c9d2257f3e2efa3aa162
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2022-09-26 01:02:52 +0300

    adds Job Description Assignment model files

---
+++ database/factories/JobDescriptionAssignmentFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\JobDescriptionAssignment>
+ */
+class JobDescriptionAssignmentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
