b0ad5afc7346fc7f11f9eb87bc15183efbcefca5
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2022-10-03 23:49:53 +0300

    fixes 419 error in login

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if($this->app->environment('production') || $this->app->environment('staging'))
+        {
+            URL::forceScheme('https');
+        }
     }
 }
