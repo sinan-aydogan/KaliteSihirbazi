ef30f71f6b1dd158cfc819edbe2eda1d1824854d
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2022-10-09 19:34:55 +0300

    adds measurement device db fields and relations

--- database/migrations/2022_08_30_075949_create_measurement_devices_table.php
+++ database/migrations/2022_08_30_075949_create_measurement_devices_table.php
@@ -15,6 +15,18 @@ return new class extends Migration
     {
         Schema::create('measurement_devices', function (Blueprint $table) {
             $table->id();
+            $table->string('code',10)->unique();
+            $table->string('brand',255)->nullable();
+            $table->string('model',255)->nullable();
+            $table->string('serial_no',255)->nullable();
+            $table->json('properties')->nullable();
+            $table->date('purchase_date')->nullable();
+            $table->string('purchase_price')->nullable();
+            $table->string('purchase_price_unit')->nullable();
+            $table->foreignId('device_supervisor_id')->nullable();
+            $table->foreignId('calibration_supervisor_id')->nullable();
+            $table->foreignIdFor(\App\Models\Department::class)->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
