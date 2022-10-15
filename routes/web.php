57f0f696232fca51398159f448669198a5122c3b
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2022-10-16 01:46:40 +0300

    updates routes - adds calibration firms route - updates other routes for changed directory structure

--- routes/web.php
+++ routes/web.php
@@ -53,12 +53,12 @@ Route::middleware([
         ['uri'=>'customer', 'model'=>'customer', 'controller'=>\App\Http\Controllers\CustomerController::class],
         ['uri'=>'supplier', 'model'=>'supplier', 'controller'=>\App\Http\Controllers\SupplierController::class],
         ['uri'=>'audit-firm', 'model'=>'auditFirm', 'controller'=>\App\Http\Controllers\AuditFirmController::class],
-        ['uri'=>'employee', 'model'=>'employee', 'controller'=>\App\Http\Controllers\EmployeeController::class],
-        ['uri'=>'job-description', 'model'=>'jobDescription', 'controller'=>\App\Http\Controllers\JobDescriptionController::class],
-        ['uri'=>'job-description-assignment', 'model'=>'jobDescriptionAssignment', 'controller'=>\App\Http\Controllers\JobDescriptionAssignmentController::class],
-        ['uri'=>'education', 'model'=>'education', 'controller'=>\App\Http\Controllers\EducationController::class],
-        ['uri'=>'education-plan', 'model'=>'educationPlan', 'controller'=>\App\Http\Controllers\EducationPlanController::class],
-        ['uri'=>'take-time-off', 'model'=>'takeTime', 'controller'=>\App\Http\Controllers\TakeTimeOffController::class],
+        ['uri'=>'employee', 'model'=>'employee', 'controller'=> \App\Http\Controllers\HumanResources\Employee\EmployeeController::class],
+        ['uri'=>'job-description', 'model'=>'jobDescription', 'controller'=> \App\Http\Controllers\HumanResources\JobDescription\JobDescriptionController::class],
+        ['uri'=>'job-description-assignment', 'model'=>'jobDescriptionAssignment', 'controller'=> \App\Http\Controllers\HumanResources\JobDescription\JobDescriptionAssignmentController::class],
+        ['uri'=>'education', 'model'=>'education', 'controller'=> \App\Http\Controllers\HumanResources\Education\EducationController::class],
+        ['uri'=>'education-plan', 'model'=>'educationPlan', 'controller'=> \App\Http\Controllers\HumanResources\Education\EducationPlanController::class],
+        ['uri'=>'take-time-off', 'model'=>'takeTime', 'controller'=> \App\Http\Controllers\HumanResources\Employee\TakeTimeOffController::class],
         ['uri'=>'problem', 'model'=>'problem', 'controller'=>App\Http\Controllers\ProblemController::class],
         ['uri'=>'capa-action', 'model'=>'capaAction', 'controller'=>\App\Http\Controllers\CapaActionController::class],
         ['uri'=>'product', 'model'=>'product', 'controller'=>\App\Http\Controllers\ProductController::class],
@@ -69,10 +69,11 @@ Route::middleware([
         ['uri'=>'document-revision-request', 'model'=>'documentRevisionRequest', 'controller'=>\App\Http\Controllers\DocumentRevisionRequestController::class],
         ['uri'=>'document-distribution-point', 'model'=>'documentDistributionPoint', 'controller'=>\App\Http\Controllers\DistributionPointController::class],
         ['uri'=>'document-action', 'model'=>'documentAction', 'controller'=>\App\Http\Controllers\DocumentActionController::class],
-        ['uri'=>'measurement-device', 'model'=>'measurementDevice', 'controller'=>\App\Http\Controllers\MeasurementDeviceController::class],
-        ['uri'=>'measurement-device-type', 'model'=>'measurementDeviceType', 'controller'=>\App\Http\Controllers\MeasurementDeviceTypeController::class],
-        ['uri'=>'calibration-task', 'model'=>'calibrationTask', 'controller'=>\App\Http\Controllers\CalibrationTaskController::class],
-        ['uri'=>'measurement-device-action', 'model'=>'measurementDeviceAction', 'controller'=>\App\Http\Controllers\MeasurementDeviceActionController::class],
+        ['uri'=>'measurement-device', 'model'=>'measurementDevice', 'controller'=> \App\Http\Controllers\MeasurementDevice\MeasurementDeviceController::class],
+        ['uri'=>'measurement-device-type', 'model'=>'measurementDeviceType', 'controller'=> \App\Http\Controllers\MeasurementDevice\MeasurementDeviceTypeController::class],
+        ['uri'=>'measurement-device-calibration', 'model'=>'measurementDeviceCalibrationTask', 'controller'=>\App\Http\Controllers\MeasurementDevice\Calibration\MeasurementDeviceCalibrationTaskController::class],
+        ['uri'=>'calibration-firm', 'model'=>'calibrationFirm', 'controller'=> \App\Http\Controllers\MeasurementDevice\Calibration\CalibrationFirmController::class],
+        ['uri'=>'measurement-device-action', 'model'=>'measurementDeviceAction', 'controller'=> \App\Http\Controllers\MeasurementDevice\Action\MeasurementDeviceActionController::class],
         ['uri'=>'audit', 'model'=>'audit', 'controller'=>\App\Http\Controllers\AuditController::class],
         ['uri'=>'certificate', 'model'=>'certificate', 'controller'=>\App\Http\Controllers\CertificateController::class],
         ['uri'=>'standard', 'model'=>'standard', 'controller'=>\App\Http\Controllers\StandardController::class],
@@ -98,15 +99,20 @@ Route::middleware([
     }
 
     /*Employee Management Pages*/
-    Route::get('employee/{employee}/personal-info', [\App\Http\Controllers\Employee\PersonalInfo::class, 'index'])->name('employee-personal-info.index');
-    Route::get('employee/{employee}/employment-info', [\App\Http\Controllers\Employee\EmploymentInfo::class, 'index'])->name('employee-employment-info.index');
-    Route::get('employee/{employee}/jd-assignment', [\App\Http\Controllers\Employee\JobAssignmentController::class, 'index'])->name('employee-jd-assignment.index');
-    Route::get('employee/{employee}/time-off', [\App\Http\Controllers\Employee\TimeOffController::class, 'index'])->name('employee-time-off.index');
-    Route::get('employee/{employee}/debt', [\App\Http\Controllers\Employee\DebtController::class, 'index'])->name('employee-debt.index');
-    Route::get('employee/{employee}/education', [\App\Http\Controllers\Employee\EducationController::class, 'index'])->name('employee-education.index');
+    Route::get('employee/{employee}/personal-info', [\App\Http\Controllers\HumanResources\Employee\PersonalInfo::class, 'index'])->name('employee-personal-info.index');
+    Route::get('employee/{employee}/employment-info', [\App\Http\Controllers\HumanResources\Employee\EmploymentInfo::class, 'index'])->name('employee-employment-info.index');
+    Route::get('employee/{employee}/jd-assignment', [\App\Http\Controllers\HumanResources\Employee\JobAssignmentController::class, 'index'])->name('employee-jd-assignment.index');
+    Route::get('employee/{employee}/time-off', [\App\Http\Controllers\HumanResources\Employee\TimeOffController::class, 'index'])->name('employee-time-off.index');
+    Route::get('employee/{employee}/debt', [\App\Http\Controllers\HumanResources\Employee\DebtController::class, 'index'])->name('employee-debt.index');
+    Route::get('employee/{employee}/education', [\App\Http\Controllers\HumanResources\Employee\EducationController::class, 'index'])->name('employee-education.index');
 
     /*Warehouse Setting Pages*/
     Route::resource('warehouse-type', \App\Http\Controllers\Warehouse\WarehouseTypeController::class);
+
+    /*Measurement Device Management Pages*/
+    Route::get('measurement-device/{measurementDevice}/device-info', [\App\Http\Controllers\MeasurementDevice\DeviceInfoController::class, 'index'])->name('measurement-device-info.index');
+    Route::get('measurement-device/{measurementDevice}/calibration-tasks', [\App\Http\Controllers\MeasurementDevice\Calibration\CalibrationTaskController::class, 'index'])->name('measurement-device-calibrations.index');
+    Route::get('measurement-device/{measurementDevice}/device-actions', [\App\Http\Controllers\MeasurementDevice\Action\DeviceActionController::class, 'index'])->name('measurement-device-actions.index');
 });
 
 // Test Route
