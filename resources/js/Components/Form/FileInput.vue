8a38027c1e7df9802097ac038654bb142b6d41fb
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2023-04-05 23:41:34 +0300

    Adds new form component: FileInput

---
+++ resources/js/Components/Form/FileInput.vue
@@ -0,0 +1,16 @@
+<template>
+<input type="file" id="file" :model-value="modelValue" @change="$emit('update:modelValue', file)">
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+defineProps({
+    modelValue: {
+        type: File,
+        default: null
+    }
+})
+
+const file = ref(null)
+</script>
