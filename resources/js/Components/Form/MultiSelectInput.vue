9ab88058c772bac5f80440ecb8518757cfdee6e6
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2023-05-10 23:20:34 +0300

    Tıklanabilir seçin alanını genişletir

--- resources/js/Components/Form/MultiSelectInput.vue
+++ resources/js/Components/Form/MultiSelectInput.vue
@@ -31,7 +31,7 @@ const emit = defineEmits(['update:modelValue', 'changed'])
 
 /*Outside click*/
 const target = ref();
-onClickOutside(target, (event) => showOptions.value = false);
+onClickOutside(target, () => showOptions.value = false);
 
 const showOptions = ref(false)
 
@@ -58,25 +58,33 @@ const updateValue = (item) => {
 
 <template>
     <div class="relative" ref="target">
-        <div class="flex flex-wrap items-center px-2 min-h-[2.5rem] border border-slate-300 rounded-lg py-2 gap-2 cursor-pointer select-none" @click="showOptions=!showOptions">
+        <div
+            class="flex flex-wrap items-center px-2 min-h-[2.5rem] border border-slate-300 rounded-lg py-2 gap-2 cursor-pointer select-none"
+            @click="showOptions=!showOptions">
             <template v-for="item in selectedItems">
                 <badge>
                     <div class="flex items-center gap-2">
                         <span v-text="item[optionLabel]" class="whitespace-nowrap"></span>
-                        <font-awesome-icon icon="times" class="ml-2 cursor-pointer hover:text-red-500" @click="deleteItem(item)"/>
+                        <font-awesome-icon icon="times" class="ml-2 cursor-pointer hover:text-red-500"
+                                           @click="deleteItem(item)"/>
                     </div>
                 </badge>
             </template>
         </div>
         <!--Options-->
-        <div v-if="showOptions && options.length>0" class="absolute flex flex-col min-w-[10rem] bg-white items-center border border-slate-300 rounded-lg mt-1 z-50 shadow-md">
+        <div v-if="showOptions && options.length>0"
+             class="absolute flex flex-col min-w-[10rem] bg-white items-center border border-slate-300 rounded-lg mt-1 z-50 shadow-md">
             <template v-for="option in options">
-                <div class="flex items-center w-full px-4 h-10 border-b last:border-b-0 border-slate-300">
-                    <div class="flex items-center">
-                        <input type="checkbox" class="form-checkbox rounded-md h-5 w-5 text-blue-600" @input="updateValue(option)" :checked="selectedItems.find(i=>i[optionKey] === option[optionKey])" :id="option[optionKey]" :value="option[optionKey]">
-                        <label :for="option[optionKey]" class="ml-3 text-sm text-gray-600">{{ option[optionLabel] }}</label>
-                    </div>
-                </div>
+                <label :for="option[optionKey]"
+                       class="flex items-center w-full px-4 h-10 border-b last:border-b-0 border-slate-300">
+                    <input type="checkbox" class="form-checkbox rounded-md h-5 w-5 text-blue-600 mr-2"
+                           @input="updateValue(option)"
+                           :checked="selectedItems.find(i=>i[optionKey] === option[optionKey])"
+                           :id="option[optionKey]"
+                           :value="option[optionKey]"
+                    >
+                    {{ option[optionLabel] }}
+                </label>
             </template>
         </div>
     </div>
