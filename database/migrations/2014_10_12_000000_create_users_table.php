4a06d22e63134dcf7e71f2404b58262bec9af237
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2024-02-25 11:31:09 +0300

    feat: adds system default language in env

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('locale')->default(env('LOCALE'));
+            $table->string('locale')->default(env('DEFAULT_LOCALE', 'tr'));
             $table->rememberToken();
             $table->timestamps();
         });
