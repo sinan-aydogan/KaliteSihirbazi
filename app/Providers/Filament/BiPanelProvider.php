7755a34dbfedd9f6e76308950abbe07f6089a92f
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2024-02-25 11:30:35 +0300

    feat: adds settings panel route

--- app/Providers/Filament/BiPanelProvider.php
+++ app/Providers/Filament/BiPanelProvider.php
@@ -3,10 +3,13 @@
 namespace App\Providers\Filament;
 
 use Althinect\FilamentSpatieRolesPermissions\FilamentSpatieRolesPermissionsPlugin;
+use App\Filament\SystemManagement\Resources\Bm\BusinessBranchResource;
 use App\Http\Middleware\LocaleMiddleware;
+use Filament\Facades\Filament;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Navigation\MenuItem;
 use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
@@ -18,6 +21,7 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 
 class BiPanelProvider extends PanelProvider
@@ -59,6 +63,14 @@ class BiPanelProvider extends PanelProvider
             ])
             ->topNavigation()
             ->plugins([
+            ])
+            ->userMenuItems([
+                MenuItem::make()
+                    ->visible(fn (): bool => Gate::check('viewSystemSettingsPanel'))
+                    ->label('Settings')
+                    ->url(fn (): string => Filament::getPanel('system-management')->getUrl())
+                    ->icon('heroicon-o-cog-6-tooth'),
+                // ...
             ]);
     }
 }
