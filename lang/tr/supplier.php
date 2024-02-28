274744b64d3b1590dee7478b1dc8b9a46d1d7a7e
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2024-02-28 22:42:27 +0300

    fix: changes from Tedarikçi Tipi to Tedarikçi Türü

--- lang/tr/supplier.php
+++ lang/tr/supplier.php
@@ -2,10 +2,10 @@
 
 return array(
     'type' => array(
-        'model_label' => 'Tedarikçi Tipi|Tedarikçi Tipleri',
-        'code' => 'Tedarikçi Tipi Kodu',
-        'code_hint' => 'Tedarikçi kodu benzersiz ve azami 10 karakter',
-        'name' => 'Tedarikçi Tipi Adı',
-        'description' => 'Tedarikçi Tipi Açıklaması',
+        'model_label' => 'Tedarikçi Türü|Tedarikçi Türleri',
+        'code' => 'Tedarikçi Türü Kodu',
+        'code_hint' => 'Tedarikçi türü kodu benzersiz ve azami 10 karakter',
+        'name' => 'Tedarikçi Türü Adı',
+        'description' => 'Tedarikçi Türü Açıklaması',
     ),
 );
