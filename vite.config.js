07deef362be9af88ea3c8d49f1764a33da3e9e58
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2025-08-20 21:05:25 +0300

    Tailwindcss4'ü postcss ile kullanır

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     plugins: [
@@ -17,6 +16,5 @@ export default defineConfig({
                 },
             },
         }),
-        tailwindcss(),
     ],
 });
